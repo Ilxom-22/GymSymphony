f684e8d461a0d0e181c3d15ae89b315b8e50cdc4
Ilxom <ilxomkarimjonov22@gmail.com>
2024-06-08 17:32:52 +0500

    Add user, admin and member entity type configurations

--- src/Gymphony.Backend/Gymphony.Persistence/DataContexts/AppDbContext.cs
+++ src/Gymphony.Backend/Gymphony.Persistence/DataContexts/AppDbContext.cs
@@ -1,9 +1,16 @@
+using Gymphony.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Gymphony.Persistence.DataContexts;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
+    public DbSet<User> Users => Set<User>();
+
+    public DbSet<Admin> Admins => Set<Admin>();
+
+    public DbSet<Member> Members => Set<Member>();
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
