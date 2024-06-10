f2d01943bbbdfe779e842b4084ac4ebcc4e63e3a
Ilxom <ilxomkarimjonov22@gmail.com>
2024-06-10 12:14:52 +0500

    Add access tokens migration

--- src/Gymphony.Backend/Gymphony.Persistence/Migrations/20240610071137_AccessTokens_Migration.Designer.cs
+++ src/Gymphony.Backend/Gymphony.Persistence/Migrations/20240610071137_AccessTokens_Migration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Gymphony.Persistence.DataContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Gymphony.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240610071137_AccessTokens_Migration")]
+    partial class AccessTokens_Migration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
