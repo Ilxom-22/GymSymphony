25926abb55ef198579ad72b9bb4419ec542a07ac
Ilxom <ilxomkarimjonov22@gmail.com>
2024-06-13 13:08:40 +0500

    Add Notification histories migration

---
+++ src/Gymphony.Backend/Gymphony.Persistence/Migrations/20240613080754_NotificationHistories_Migration.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gymphony.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class NotificationHistories_Migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NotificationHistories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    RecipientId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TemplateId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    NotificationMethod = table.Column<int>(type: "integer", nullable: false),
+                    SentDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    ErrorMessage = table.Column<string>(type: "text", nullable: true),
+                    CreatedTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    ModifiedTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotificationHistories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_NotificationHistories_NotificationTemplates_TemplateId",
+                        column: x => x.TemplateId,
+                        principalTable: "NotificationTemplates",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NotificationHistories_TemplateId",
+                table: "NotificationHistories",
+                column: "TemplateId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NotificationHistories");
+        }
+    }
+}
