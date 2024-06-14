8b2a14fb5f23ea466dc71e57b46113fc6db01861
Ilxom <ilxomkarimjonov22@gmail.com>
2024-06-15 00:09:04 +0500

    Refactor notification message, create validator and notification history mapper

--- src/Gymphony.Backend/Gymphony.Application/Common/Notifications/Models/Dtos/NotificationMessage.cs
+++ src/Gymphony.Backend/Gymphony.Application/Common/Notifications/Models/Dtos/NotificationMessage.cs
@@ -1,25 +1,28 @@
+using System.Text;
 using Gymphony.Domain.Entities;
 using Gymphony.Domain.Enums;
 
-namespace Gymphony.Application.Common.Notifications.Models;
+namespace Gymphony.Application.Common.Notifications.Models.Dtos;
 
-public abstract class NotificationMessage
+public class NotificationMessage
 {
-    public NotificationTemplate Template { get; set; } = default!;
+    public Guid TemplateId { get; set; }
 
     public User Recipient { get; set; } = default!;
 
-    public string Title { get; set; } = default!;
+    public StringBuilder Title { get; set; } = default!;
 
-    public string Content { get; set; } = default!;
+    public StringBuilder Content { get; set; } = default!;
 
-    public NotificationStatus Status { get; set; }
+    public NotificationStatus Status { get; set; } = NotificationStatus.Pending;
 
     public NotificationMethod NotificationMethod { get; set; }
 
     public bool IsRendered { get; set; }
 
-    public DateTimeOffset? SentTime { get; set; }
+    public DateTimeOffset SentTime { get; set; }
+
+    public string? ErrorMessage { get; set; }
 
     public Dictionary<string, string> Variables { get; set; } = new();
 }
