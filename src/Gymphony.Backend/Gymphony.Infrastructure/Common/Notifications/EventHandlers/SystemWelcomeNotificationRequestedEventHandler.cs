a7df7787776279ea163e00e9605be10f3ad8131d
Ilxom <ilxomkarimjonov22@gmail.com>
2024-06-16 18:31:13 +0500

    Refactor system welcome notification requested event handler

--- src/Gymphony.Backend/Gymphony.Infrastructure/Common/Notifications/EventHandlers/SystemWelcomeNotificationRequestedEventHandler.cs
+++ src/Gymphony.Backend/Gymphony.Infrastructure/Common/Notifications/EventHandlers/SystemWelcomeNotificationRequestedEventHandler.cs
@@ -1,49 +1,33 @@
-using System.Text;
 using Gymphony.Application.Common.EventBus.Brokers;
-using Gymphony.Application.Common.Exceptions;
+using Gymphony.Application.Common.Notifications.Commands;
 using Gymphony.Application.Common.Notifications.Events;
-using Gymphony.Application.Common.Notifications.Models.Dtos;
 using Gymphony.Domain.Common.Events;
-using Gymphony.Domain.Common.Queries;
 using Gymphony.Domain.Constants;
-using Gymphony.Domain.Entities;
 using Gymphony.Domain.Enums;
-using Gymphony.Persistence.Repositories.Interfaces;
-using Microsoft.Extensions.DependencyInjection;
+using MediatR;
 
 namespace Gymphony.Infrastructure.Common.Notifications.EventHandlers;
 
 public class SystemWelcomeNotificationRequestedEventHandler(
     IEventBusBroker eventBusBroker,
-    IServiceProvider serviceProvider)
+    IMediator mediator)
     : IEventHandler<SystemWelcomeNotificationRequestedEvent>
 {
     public async Task Handle(SystemWelcomeNotificationRequestedEvent notification, CancellationToken cancellationToken)
     {
-        await using var scope = serviceProvider.CreateAsyncScope();
-        var notificationTemplateRepository = scope.ServiceProvider.GetRequiredService<INotificationTemplateRepository>();
-        
-        var notificationTemplate = await notificationTemplateRepository
-            .GetByType(NotificationType.SystemWelcome, 
-                new QueryOptions(QueryTrackingMode.AsNoTracking), cancellationToken)
-            ?? throw new EntityNotFoundException<NotificationTemplate>($"Requested notification template not found.");
+        var message = await mediator.Send(new RetrieveTemplateAsNotificationMessageCommand
+            { TemplateType = NotificationType.SystemWelcome }, cancellationToken);
 
-        var notificationMessage = new NotificationMessage
+        message.NotificationMethod = NotificationMethod.Email;
+        message.Recipient = notification.Recipient;
+        message.Status = NotificationStatus.Pending;
+        message.Variables = new()
         {
-            TemplateId = notificationTemplate.Id,
-            Recipient = notification.Recipient,
-            Title = new StringBuilder(notificationTemplate.Title),
-            Content = new StringBuilder(notificationTemplate.Content),
-            NotificationMethod = NotificationMethod.Email,
-            Status = NotificationStatus.Pending,
-            Variables = new()
-            {
-                { NotificationPlaceholderConstants.FirstName, notification.Recipient.FirstName },
-                { NotificationPlaceholderConstants.CompanyName, NotificationPlaceholderConstants.CompanyNameVariable }
-            }
+            { NotificationPlaceholderConstants.FirstName, notification.Recipient.FirstName },
+            { NotificationPlaceholderConstants.CompanyName, NotificationPlaceholderConstants.CompanyNameVariable }
         };
 
         await eventBusBroker.PublishLocalAsync(new NotificationMessageGeneratedEvent 
-            { Message = notificationMessage });
+            { Message = message });
     }
 }
