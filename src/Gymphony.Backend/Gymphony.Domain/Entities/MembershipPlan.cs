738080297ac3cd676254bd0d60e7264613f9ccaf
Ilxom <ilxomkarimjonov22@gmail.com>
2024-06-23 21:59:33 +0500

    Create membership plan entity and content status enum

---
+++ src/Gymphony.Backend/Gymphony.Domain/Entities/MembershipPlan.cs
@@ -0,0 +1,28 @@
+using Gymphony.Domain.Common.Entities;
+using Gymphony.Domain.Enums;
+
+namespace Gymphony.Domain.Entities;
+
+public class MembershipPlan : AuditableEntity, ICreationAuditableEntity,
+    IModificationAuditableEntity
+{
+    public string Name { get; set; } = default!;
+
+    public string Description { get; set; } = default!;
+
+    public int DurationInDays { get; set; }
+
+    public ContentStatus Status { get; set; } = ContentStatus.Draft;
+
+    public DateOnly? ActivationDate { get; set; }
+
+    public decimal Price { get; set; }
+    
+    public Guid? CreatedByUserId { get; set; }
+    
+    public Guid? ModifiedByUserId { get; set; }
+    
+    public Admin? CreatedBy { get; set; }
+
+    public Admin? ModifiedBy { get; set; }
+}
