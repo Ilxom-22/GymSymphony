c79d1a5949ef10c31503cc142da79a7b3911e42d
Ilxom <ilxomkarimjonov22@gmail.com>
2024-06-24 06:11:54 +0500

    Add MembershipPlans infrastructure to DI

--- src/Gymphony.Backend/Gymphony.Api/Configurations/HostConfigurations.cs
+++ src/Gymphony.Backend/Gymphony.Api/Configurations/HostConfigurations.cs
@@ -15,6 +15,7 @@ public static partial class HostConfigurations
             .AddIdentityInfrastructure()
             .AddUsersInfrastructure()
             .AddNotificationsInfrastructure()
+            .AddMembershipPlansInfrastructure()
             .AddValidators()
             .AddMappers();
         
