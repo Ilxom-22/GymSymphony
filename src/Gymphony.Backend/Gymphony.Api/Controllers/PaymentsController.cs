aa0993cbdd83b79e32e341a2c7b871a7b4766df7
Ilxom <ilxomkarimjonov22@gmail.com>
2024-06-27 05:56:04 +0500

    Add cutomer portal and webhook endpoints to payments controller

--- src/Gymphony.Backend/Gymphony.Api/Controllers/PaymentsController.cs
+++ src/Gymphony.Backend/Gymphony.Api/Controllers/PaymentsController.cs
@@ -1,18 +1,76 @@
+using Gymphony.Application.Common.EventBus.Brokers;
 using Gymphony.Application.Common.Payments.Commands;
+using Gymphony.Application.Common.Payments.Events;
+using Gymphony.Application.Common.Payments.Models.Settings;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Stripe;
 
 namespace Gymphony.Api.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class PaymentsController(IMediator mediator) : ControllerBase
+public class PaymentsController(IMediator mediator, 
+    IEventBusBroker eventBusBroker,
+    IOptions<StripeSettings> stripeSettings) : ControllerBase
 {
+    private readonly StripeSettings _stripeSettings = stripeSettings.Value;
+    
+    [Authorize(Roles = "Member")]
     [HttpPost("create-checkout-session")]
     public async ValueTask<IActionResult> CreateCheckoutSession([FromBody] CreateCheckoutSessionCommand createCheckoutSessionCommand, CancellationToken cancellationToken)
     {
         var sessionId = await mediator.Send(createCheckoutSessionCommand, cancellationToken);
 
         return Ok(sessionId);
+    }
+    
+    [Authorize(Roles = "Member")]
+    [HttpPost("customer-portal")]
+    public async ValueTask<IActionResult> CreateCustomerPortalSession([FromBody] CreateBillingPortalSessionCommand createBillingPortalSessionCommand, 
+        CancellationToken cancellationToken)
+    {
+        var sessionUrl = await mediator.Send(createBillingPortalSessionCommand, cancellationToken);
+
+        return Ok(sessionUrl);
+    }
+
+    [HttpPost("webhook")]
+    public async ValueTask<IActionResult> WebHook()
+    {
+        var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+
+        try
+        {
+            var stripeEvent = EventUtility.ConstructEvent(json, 
+                Request.Headers["Stripe-Signature"], 
+                _stripeSettings.WebHookSecret);
+
+            switch (stripeEvent.Type)
+            {
+                case Events.CheckoutSessionCompleted:
+                {
+                    var session = stripeEvent.Data.Object as Stripe.Checkout.Session;
+                    await eventBusBroker
+                        .PublishLocalAsync(new StripeCheckoutSessionCompletedEvent { Session = session! });
+                    break;
+                }
+                case Events.InvoicePaymentSucceeded:
+                {
+                    var invoice = stripeEvent.Data.Object as Invoice;
+                    await eventBusBroker
+                        .PublishLocalAsync(new StripeInvoicePaymentSucceededEvent { Invoice = invoice! });
+                    break;
+                }
+            }
+
+            return Accepted();
+        }
+        catch (StripeException ex)
+        {
+            return BadRequest();
+        }
     }
 }
