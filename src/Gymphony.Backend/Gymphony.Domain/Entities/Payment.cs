0805116c2e3ee057fc26eafec51529ced14ac64c
Ilxom <ilxomkarimjonov22@gmail.com>
2024-06-28 22:17:51 +0500

    Create payment entity and its configuration

---
+++ src/Gymphony.Backend/Gymphony.Domain/Entities/Payment.cs
@@ -0,0 +1,14 @@
+using Gymphony.Domain.Common.Entities;
+
+namespace Gymphony.Domain.Entities;
+
+public class Payment : Entity
+{
+    public Guid MemberId { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public DateTimeOffset Date { get; set; }
+
+    public Member? Member { get; set; }
+}
