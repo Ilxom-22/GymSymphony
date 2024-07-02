ae011e070c681c4397accbb67c63872283fdedc8
Ilxom <ilxomkarimjonov22@gmail.com>
2024-07-02 14:20:51 +0500

    Create products configurations

--- src/Gymphony.Backend/Gymphony.Persistence/DataContexts/AppDbContext.cs
+++ src/Gymphony.Backend/Gymphony.Persistence/DataContexts/AppDbContext.cs
@@ -5,23 +5,33 @@ namespace Gymphony.Persistence.DataContexts;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
+    #region Identity
     public DbSet<User> Users => Set<User>();
-
     public DbSet<Admin> Admins => Set<Admin>();
-
     public DbSet<Member> Members => Set<Member>();
-
     public DbSet<AccessToken> AccessTokens => Set<AccessToken>();
-
     public DbSet<RefreshToken> RefreshTokens => Set<RefreshToken>();
+    
+    #endregion
 
+    #region Notifications
     public DbSet<NotificationTemplate> NotificationTemplates => Set<NotificationTemplate>();
-
     public DbSet<NotificationHistory> NotificationHistories => Set<NotificationHistory>();
-
     public DbSet<VerificationToken> VerificationTokens => Set<VerificationToken>();
 
+    #endregion
+
+    #region Products
+    public DbSet<Product> Products => Set<Product>();
     public DbSet<MembershipPlan> MembershipPlans => Set<MembershipPlan>();
+    public DbSet<Course> Courses => Set<Course>();
+
+    #endregion
+
+    #region Subscription & Payments
+    public DbSet<Payment> Payments => Set<Payment>();
+
+    #endregion
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- src/Gymphony.Backend/Gymphony.Persistence/EntityConfigurations/MembershipPlanConfiguration.cs
+++ src/Gymphony.Backend/Gymphony.Persistence/EntityConfigurations/MembershipPlanConfiguration.cs
@@ -8,23 +8,6 @@ public class MembershipPlanConfiguration : IEntityTypeConfiguration<MembershipPl
 {
     public void Configure(EntityTypeBuilder<MembershipPlan> builder)
     {
-        builder.HasKey(plan => plan.Id);
-
-        builder.HasIndex(plan => plan.Name).IsUnique();
-        builder.Property(plan => plan.Name).HasMaxLength(256);
-        builder.Property(plan => plan.Description).IsRequired().HasMaxLength(2048);
-        builder.Property(plan => plan.Price).IsRequired().HasColumnType("numeric(18,2)");
-
-        builder.OwnsOne(plan => plan.StripeDetails);
-
-        builder.HasOne<Admin>(plan => plan.CreatedBy)
-            .WithMany()
-            .HasForeignKey(plan => plan.CreatedByUserId)
-            .OnDelete(DeleteBehavior.SetNull);
-
-        builder.HasOne<Admin>(plan => plan.ModifiedBy)
-            .WithMany()
-            .HasForeignKey(plan => plan.ModifiedByUserId)
-            .OnDelete(DeleteBehavior.SetNull);
+        
     }
 }
