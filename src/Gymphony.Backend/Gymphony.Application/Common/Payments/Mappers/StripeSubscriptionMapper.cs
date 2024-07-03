59f3ead368f0f2318da2391437a931c90158c63a
Ilxom <ilxomkarimjonov22@gmail.com>
2024-07-03 18:48:36 +0500

    Refactor stripe dto models and mappers

--- src/Gymphony.Backend/Gymphony.Application/Common/Payments/Mappers/StripeSubscriptionMapper.cs
+++ src/Gymphony.Backend/Gymphony.Application/Common/Payments/Mappers/StripeSubscriptionMapper.cs
@@ -9,22 +9,15 @@ public class StripeSubscriptionMapper : Profile
 {
     public StripeSubscriptionMapper()
     {
-        CreateMap<Stripe.Checkout.Session, StripeSubscriptionDto>()
-            .ForMember(dest => dest.ProductType,
-                opt => opt.MapFrom(src => Enum.Parse<ProductType>(src.Subscription.Items.Data[0].Plan.Product.Metadata["type"])))
-            .ForMember(dest => dest.ProductId, opt => opt.MapFrom(src => src.Subscription.Items.Data[0].Plan.ProductId))
-            .ForMember(dest => dest.PaymentAmount, opt => opt.MapFrom(src => src.AmountTotal))
-            .ForMember(dest => dest.SubscriptionStartDate, opt => opt.MapFrom(src => src.Subscription.CurrentPeriodStart))
-            .ForMember(dest => dest.SubscriptionEndDate, opt => opt.MapFrom(src => src.Subscription.CurrentPeriodEnd))
-            .ForMember(dest => dest.PaymentDate, opt => opt.MapFrom(src => DateTimeOffset.UtcNow));
-        
-        CreateMap<Invoice, StripeSubscriptionDto>()
-            .ForMember(dest => dest.ProductType,
-                opt => opt.MapFrom(src => Enum.Parse<ProductType>(src.Subscription.Items.Data[0].Plan.Product.Metadata["type"])))
-            .ForMember(dest => dest.ProductId, opt => opt.MapFrom(src => src.Subscription.Items.Data[0].Plan.ProductId))
+        CreateMap<Invoice, StripeInvoiceDto>()
             .ForMember(dest => dest.PaymentAmount, opt => opt.MapFrom(src => src.AmountPaid))
-            .ForMember(dest => dest.SubscriptionStartDate, opt => opt.MapFrom(src => src.Subscription.CurrentPeriodStart))
-            .ForMember(dest => dest.SubscriptionEndDate, opt => opt.MapFrom(src => src.Subscription.CurrentPeriodEnd))
             .ForMember(dest => dest.PaymentDate, opt => opt.MapFrom(src => DateTimeOffset.UtcNow));
+
+        CreateMap<Subscription, StripeSubscriptionDto>()
+            .ForMember(dest => dest.ProductType,
+                opt => opt.MapFrom(src => Enum.Parse<ProductType>(src.Items.Data[0].Plan.Product.Metadata["type"])))
+            .ForMember(dest => dest.ProductId, opt => opt.MapFrom(src => src.Items.Data[0].Plan.ProductId))
+            .ForMember(dest => dest.SubscriptionStartDate, opt => opt.MapFrom(src => src.CurrentPeriodStart))
+            .ForMember(dest => dest.SubscriptionEndDate, opt => opt.MapFrom(src => src.CurrentPeriodEnd));
     }
 }
