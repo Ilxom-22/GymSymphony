8ddd5f28595bf7c691b245d63487dcf6cd5a3d53
Ilxom <ilxomkarimjonov22@gmail.com>
2024-07-04 16:11:19 +0500

    Create UpdateStripePrice command and handler

---
+++ src/Gymphony.Backend/Gymphony.Infrastructure/Common/Payments/CommandHandlers/UpdateStripePriceCommandHandler.cs
@@ -0,0 +1,28 @@
+using AutoMapper;
+using Force.DeepCloner;
+using Gymphony.Application.Common.Payments.Brokers;
+using Gymphony.Application.Common.Payments.Commands;
+using Gymphony.Domain.Common.Commands;
+
+namespace Gymphony.Infrastructure.Common.Payments.CommandHandlers;
+
+public class UpdateStripePriceCommandHandler(IStripePriceBroker stripePriceBroker, IMapper mapper) : ICommandHandler<UpdateStripePriceCommand, string>
+{
+    public async Task<string> Handle(UpdateStripePriceCommand request, CancellationToken cancellationToken)
+    {
+        var oldPrice = await stripePriceBroker.GetByIdAsync(request.StripeDetails.PriceId, cancellationToken);
+
+        if (oldPrice.UnitAmountDecimal == request.NewPrice * 100)
+            return oldPrice.Id;
+
+        var newPrice = oldPrice.DeepClone();
+        newPrice.UnitAmountDecimal = request.NewPrice;
+        newPrice.Id = null!;
+        newPrice.Product = request.StripeDetails.ProductId;
+
+        await stripePriceBroker.DeactivateAsync(oldPrice.Id, cancellationToken);
+        newPrice = await stripePriceBroker.CreateAsync(newPrice, cancellationToken);
+        
+        return newPrice.Id;
+    }
+}
