c929b8bbed7dcb204293f6aea73d9f2f6b4ad346
Ilxom <ilxomkarimjonov22@gmail.com>
2024-07-15 10:35:44 +0500

    Update Staff Repository, add new method

--- src/Gymphony.Backend/Gymphony.Persistence/Repositories/Interfaces/IStaffRepository.cs
+++ src/Gymphony.Backend/Gymphony.Persistence/Repositories/Interfaces/IStaffRepository.cs
@@ -8,6 +8,8 @@ public interface IStaffRepository
 {
     IQueryable<Staff> Get(Expression<Func<Staff, bool>>? predicate = default, QueryOptions queryOptions = default);
 
+    ValueTask<IList<Staff>> GetByIdsAsync(ICollection<Guid> ids, CancellationToken cancellationToken = default);
+
     ValueTask<Staff> CreateAsync(Staff staff, bool saveChanges = true, CancellationToken cancellationToken = default);
 
     ValueTask<Staff> DeleteAsync(Staff staff, bool saveChanges = true, CancellationToken cancellationToken = default);
--- src/Gymphony.Backend/Gymphony.Persistence/Repositories/StaffRepository.cs
+++ src/Gymphony.Backend/Gymphony.Persistence/Repositories/StaffRepository.cs
@@ -2,15 +2,23 @@
 using Gymphony.Domain.Entities;
 using Gymphony.Persistence.DataContexts;
 using Gymphony.Persistence.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
 
 namespace Gymphony.Persistence.Repositories;
 
 public class StaffRepository(AppDbContext dbContext) : EntityRepositoryBase<AppDbContext, Staff>(dbContext), IStaffRepository
 {
-    public new IQueryable<Staff> Get(Expression<Func<Staff, bool>>? predicate, QueryOptions queryOptions)
+    public new IQueryable<Staff> Get(Expression<Func<Staff, bool>>? predicate = default, QueryOptions queryOptions = default)
     {
         return base.Get(predicate, queryOptions);
+    }
+
+    public async ValueTask<IList<Staff>> GetByIdsAsync(ICollection<Guid> ids, CancellationToken cancellationToken = default)
+    {
+        return await base.Get()
+                    .Where(staff => ids.Contains(staff.Id))
+                    .ToListAsync(cancellationToken);
     }
 
     public new ValueTask<Staff> CreateAsync(Staff staff, bool saveChanges = true, CancellationToken cancellationToken = default)
@@ -18,7 +26,7 @@ public class StaffRepository(AppDbContext dbContext) : EntityRepositoryBase<AppD
         return base.CreateAsync(staff, saveChanges, cancellationToken);
     }
 
-    public new ValueTask<Staff> DeleteAsync(Staff staff, bool saveChanges, CancellationToken cancellationToken)
+    public new ValueTask<Staff> DeleteAsync(Staff staff, bool saveChanges = true, CancellationToken cancellationToken = default)
     {
         return base.DeleteAsync(staff, saveChanges, cancellationToken);
     }
