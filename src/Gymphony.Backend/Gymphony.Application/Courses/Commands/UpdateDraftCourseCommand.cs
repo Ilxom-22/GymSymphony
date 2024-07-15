e1e4f084bca30bb2bfdc253af3bd334169682a95
Ilxom <ilxomkarimjonov22@gmail.com>
2024-07-15 10:48:37 +0500

    Add UpdateDraftCourse command and handler

---
+++ src/Gymphony.Backend/Gymphony.Application/Courses/Commands/UpdateDraftCourseCommand.cs
@@ -0,0 +1,25 @@
+﻿using Gymphony.Application.Courses.Models.Dtos;
+using Gymphony.Domain.Common.Commands;
+
+namespace Gymphony.Application.Courses.Commands;
+
+public class UpdateDraftCourseCommand : ICommand<CourseDto>
+{
+    public Guid CourseId { get; set; }
+
+    public string Name { get; set; } = default!;
+
+    public string Description { get; set; } = default!;
+
+    public string DurationUnit { get; set; } = default!;
+
+    public byte DurationCount { get; set; }
+
+    public int Capacity { get; set; }
+
+    public int SessionDurationInMinutes { get; set; }
+
+    public int EnrollmentsCountPerWeek { get; set; }
+
+    public decimal Price { get; set; }
+}
