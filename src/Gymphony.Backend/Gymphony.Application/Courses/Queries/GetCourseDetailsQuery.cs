9d24abfae56e13695a4d0d1416f7a509302a3012
Ilxom <ilxomkarimjonov22@gmail.com>
2024-07-15 10:52:15 +0500

    Add GetCourseDetails query and handler

---
+++ src/Gymphony.Backend/Gymphony.Application/Courses/Queries/GetCourseDetailsQuery.cs
@@ -0,0 +1,9 @@
+﻿using Gymphony.Application.Courses.Models.Dtos;
+using Gymphony.Domain.Common.Queries;
+
+namespace Gymphony.Application.Courses.Queries;
+
+public class GetCourseDetailsQuery : IQuery<CourseDetailsDto>
+{
+    public Guid CourseId { get; set; }
+}
