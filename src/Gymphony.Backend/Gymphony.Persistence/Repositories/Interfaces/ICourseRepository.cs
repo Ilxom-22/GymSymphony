1e1a90d3609c59b6e8ab44e2dbf33696e9e58b43
Ilxom <ilxomkarimjonov22@gmail.com>
2024-07-15 10:36:36 +0500

    Create Course repository

---
+++ src/Gymphony.Backend/Gymphony.Persistence/Repositories/Interfaces/ICourseRepository.cs
@@ -0,0 +1,20 @@
+﻿using Gymphony.Domain.Common.Queries;
+using Gymphony.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace Gymphony.Persistence.Repositories.Interfaces;
+
+public interface ICourseRepository
+{
+    IQueryable<Course> Get(Expression<Func<Course, bool>>? predicate = default, QueryOptions queryOptions = default);
+
+    ValueTask<Course?> GetByIdAsync(Guid courseId, QueryOptions queryOptoins = default, CancellationToken cancellationToken = default);
+
+    ValueTask<bool> CourseExistsAsync(string name, CancellationToken cancellationToken = default);
+
+    ValueTask<Course> CreateAsync(Course course, bool saveChanges = true, CancellationToken cancellationToken = default);
+
+    ValueTask<Course> UpdateAsync(Course course, bool saveChanges = true, CancellationToken cancellationToken = default);
+
+    ValueTask<Course> DeleteAsync(Course course, bool saveChanges = true, CancellationToken cancellationToken = default);
+}
