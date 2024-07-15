a706958dcd2a4b3911f7e2eca074dd5ecd2ed47a
Ilxom <ilxomkarimjonov22@gmail.com>
2024-07-15 10:50:06 +0500

    Add CreateCourseSchedule command, validator and handler

---
+++ src/Gymphony.Backend/Gymphony.Infrastructure/Courses/Validators/CreateCourseScheduleCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using Gymphony.Application.Courses.Commands;
+using Gymphony.Domain.Enums;
+
+namespace Gymphony.Infrastructure.Courses.Validators;
+
+public class CreateCourseScheduleCommandValidator : AbstractValidator<CreateCourseScheduleCommand>
+{
+    public CreateCourseScheduleCommandValidator()
+    {
+        RuleFor(command => command.CourseId)
+            .NotEmpty()
+            .NotEqual(Guid.Empty);
+
+        RuleFor(command => command.InstructorsIds)
+            .NotEmpty();
+
+        RuleFor(command => command.Day)
+            .Must(BeDayOfWeekEnum);
+    }
+
+    private bool BeDayOfWeekEnum(string day) =>
+         Enum.TryParse(day, false, out DayOfWeek dayOfWeek);
+}
