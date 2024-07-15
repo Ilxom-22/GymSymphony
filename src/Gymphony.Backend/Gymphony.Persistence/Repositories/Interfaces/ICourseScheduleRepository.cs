beb3948d9894ce6df7011d95c8a36db765bc8d26
Ilxom <ilxomkarimjonov22@gmail.com>
2024-07-15 10:36:52 +0500

    Add Course schedule repository

---
+++ src/Gymphony.Backend/Gymphony.Persistence/Repositories/Interfaces/ICourseScheduleRepository.cs
@@ -0,0 +1,16 @@
+﻿using Gymphony.Domain.Common.Queries;
+using Gymphony.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace Gymphony.Persistence.Repositories.Interfaces;
+
+public interface ICourseScheduleRepository
+{
+    IQueryable<CourseSchedule> Get(Expression<Func<CourseSchedule, bool>>? predicate = default, QueryOptions queryOptions = default);
+
+    ValueTask<IList<CourseSchedule>> GetByIdsAsync(ICollection<Guid> ids, CancellationToken cancellationToken = default);
+
+    ValueTask<CourseSchedule?> GetByIdAsync(Guid scheduleId, QueryOptions queryOptions = default, CancellationToken cancellationToken = default);
+
+    ValueTask<CourseSchedule> DeleteAsync(CourseSchedule courseSchedule, bool saveChanges = true, CancellationToken cancellationToken = default);
+}
