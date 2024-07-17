331025c0bc739bc47481ac8c8d3aaad65dc95cab
Ilxom <ilxomkarimjonov22@gmail.com>
2024-07-17 15:00:36 +0500

    Update course schedule enrollment repository

--- src/Gymphony.Backend/Gymphony.Persistence/Repositories/Interfaces/ICourseScheduleEnrollmentRepository.cs
+++ src/Gymphony.Backend/Gymphony.Persistence/Repositories/Interfaces/ICourseScheduleEnrollmentRepository.cs
@@ -1,9 +1,19 @@
-﻿using Gymphony.Domain.Entities;
+﻿using Gymphony.Domain.Common.Queries;
+using Gymphony.Domain.Entities;
+using System.Diagnostics;
+using System.Linq.Expressions;
 
 namespace Gymphony.Persistence.Repositories.Interfaces;
 
 public interface ICourseScheduleEnrollmentRepository
 {
+    IQueryable<CourseScheduleEnrollment> Get(Expression<Func<CourseScheduleEnrollment, bool>>? predicate = default, QueryOptions queryOptions = default);
+
     ValueTask<CourseScheduleEnrollment> CreateAsync(CourseScheduleEnrollment courseScheduleEnrollment,
         bool saveChanges = true, CancellationToken cancellationToken = default);
+
+    ValueTask<CourseScheduleEnrollment> DeleteAsync(CourseScheduleEnrollment courseScheduleEnrollment, 
+        bool saveChanges = true, CancellationToken cancellationToken = default);
+
+    ValueTask<int> BatchDeleteAsync(Expression<Func<CourseScheduleEnrollment, bool>> predicate, CancellationToken cancellationToken = default);
 }
