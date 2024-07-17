f61b645d07a9f8806c8d883fd5a0862fd3d50794
Ilxom <ilxomkarimjonov22@gmail.com>
2024-07-17 11:51:17 +0500

    Create stripe checkout session completed event and handler

---
+++ src/Gymphony.Backend/Gymphony.Application/Common/Payments/Events/StripeCheckoutSessionCompeletedEvent.cs
@@ -0,0 +1,16 @@
+﻿using Gymphony.Domain.Common.Events;
+
+namespace Gymphony.Application.Common.Payments.Events;
+
+public class StripeCheckoutSessionCompeletedEvent : EventBase
+{
+    public string SessionId { get; set; } = default!;
+
+    public string CustomerId { get; set; } = default!;
+
+    public string SubscriptionId { get; set; } = default!;
+
+    public long PaymentAmount { get; set; }
+
+    public DateTimeOffset PaymentDate { get; set; }
+}
