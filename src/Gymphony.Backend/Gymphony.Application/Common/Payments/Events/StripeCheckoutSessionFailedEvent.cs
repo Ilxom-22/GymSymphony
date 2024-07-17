b533e088909ec8cf39095b63eacbbbed64ccbf22
Ilxom <ilxomkarimjonov22@gmail.com>
2024-07-17 11:51:39 +0500

    Create stripe checkout session failed event and handler and add necessary webhooks

---
+++ src/Gymphony.Backend/Gymphony.Application/Common/Payments/Events/StripeCheckoutSessionFailedEvent.cs
@@ -0,0 +1,8 @@
+﻿using Gymphony.Domain.Common.Events;
+
+namespace Gymphony.Application.Common.Payments.Events;
+
+public class StripeCheckoutSessionFailedEvent : EventBase
+{
+    public string SessionId { get; set; } = default!;
+}
