c2f9364e0dc17ee955835067c2dabb856750fdc2
Ilxom <ilxomkarimjonov22@gmail.com>
2024-07-17 11:50:46 +0500

    Create subscribe for course endpoint, command and handler

---
+++ src/Gymphony.Backend/Gymphony.Application/Subscriptions/Commands/SubscribeForCourseCommand.cs
@@ -0,0 +1,15 @@
+﻿using Gymphony.Application.Common.Payments.Models.Dtos;
+using Gymphony.Domain.Common.Commands;
+
+namespace Gymphony.Application.Subscriptions.Commands;
+
+public class SubscribeForCourseCommand : ICommand<CheckoutSessionDto>
+{
+    public Guid CourseId { get; set; }
+
+    public ICollection<Guid> SchedulesIds { get; set; } = default!;
+
+    public string SuccessUrl { get; set; } = default!;
+
+    public string CancelUrl { get; set; } = default!;
+}
