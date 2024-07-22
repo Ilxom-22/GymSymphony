c25139a2d288e4b5e044f0fd8a00ae897ac78d1d
Ilxom <ilxomkarimjonov22@gmail.com>
2024-07-22 14:55:32 +0500

    Create Storage file repository

---
+++ src/Gymphony.Backend/Gymphony.Persistence/Repositories/StorageFileRepository.cs
@@ -0,0 +1,14 @@
+﻿using Gymphony.Domain.Entities;
+using Gymphony.Persistence.DataContexts;
+using Gymphony.Persistence.Repositories.Interfaces;
+
+namespace Gymphony.Persistence.Repositories;
+
+public class StorageFileRepository(AppDbContext dbContext) : EntityRepositoryBase<AppDbContext, StorageFile>(dbContext), IStorageFileRepository
+{
+    public new ValueTask<StorageFile> DeleteAsync(StorageFile storageFile, bool saveChanges = true, 
+        CancellationToken cancellationToken = default)
+    {
+        return base.DeleteAsync(storageFile, saveChanges, cancellationToken);
+    }
+}
