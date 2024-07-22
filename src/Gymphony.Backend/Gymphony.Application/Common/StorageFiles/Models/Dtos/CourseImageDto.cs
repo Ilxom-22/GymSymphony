04fdba919377a766fd009b78aad756df5f5a57a4
Ilxom <ilxomkarimjonov22@gmail.com>
2024-07-22 14:56:43 +0500

    Create course image dto

---
+++ src/Gymphony.Backend/Gymphony.Application/Common/StorageFiles/Models/Dtos/CourseImageDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Gymphony.Application.Common.StorageFiles.Models.Dtos;
+
+public class CourseImageDto
+{
+    public Guid CourseImageId { get; set; }
+
+    public string CourseImageUrl { get; set; } = default!;
+}
