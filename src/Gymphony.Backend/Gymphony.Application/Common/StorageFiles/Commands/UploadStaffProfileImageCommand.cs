f6451a0b9ed064ddbdfaade1deb252f85bc67d1e
Ilxom <ilxomkarimjonov22@gmail.com>
2024-07-22 16:33:49 +0500

    Create upload staff profile image command and handler

---
+++ src/Gymphony.Backend/Gymphony.Application/Common/StorageFiles/Commands/UploadStaffProfileImageCommand.cs
@@ -0,0 +1,15 @@
+﻿using Gymphony.Application.Common.StorageFiles.Models.Dtos;
+using Gymphony.Domain.Common.Commands;
+
+namespace Gymphony.Application.Common.StorageFiles.Commands;
+
+public class UploadStaffProfileImageCommand : ICommand<UserProfileImageDto>
+{
+    public string ContentType { get; set; } = default!;
+
+    public long Size { get; set; }
+
+    public Stream Source { get; set; } = default!;
+
+    public Guid StaffId { get; set; }
+}
