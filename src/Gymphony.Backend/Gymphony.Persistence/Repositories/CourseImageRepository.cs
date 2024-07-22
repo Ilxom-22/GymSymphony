5445d233000441c14877d686f6a93a7aa07276b3
Ilxom <ilxomkarimjonov22@gmail.com>
2024-07-22 14:55:56 +0500

    Add Course Images repository

---
+++ src/Gymphony.Backend/Gymphony.Persistence/Repositories/CourseImageRepository.cs
@@ -0,0 +1,35 @@
+﻿using Gymphony.Domain.Common.Queries;
+using Gymphony.Domain.Entities;
+using Gymphony.Persistence.DataContexts;
+using Gymphony.Persistence.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Gymphony.Persistence.Repositories;
+
+public class CourseImageRepository(AppDbContext dbContext) : EntityRepositoryBase<AppDbContext, CourseImage>(dbContext), ICourseImageRepository
+{
+    public new IQueryable<CourseImage> Get(Expression<Func<CourseImage, bool>>? predicate = default, QueryOptions queryOptions = default)
+    {
+        return base.Get(predicate, queryOptions);
+    }
+
+    public async ValueTask<CourseImage?> GetByIdAsync(Guid courseImageId, QueryOptions queryOptions = default, 
+        CancellationToken cancellationToken = default)
+    {
+        return await base.Get(ci => ci.Id == courseImageId, queryOptions)
+            .FirstOrDefaultAsync(cancellationToken);
+    }
+
+    public new ValueTask<CourseImage> CreateAsync(CourseImage courseImage, bool saveChanges = true,
+        CancellationToken cancellationToken = default)
+    {
+        return base.CreateAsync(courseImage, saveChanges, cancellationToken);
+    }
+
+    public new ValueTask<CourseImage> DeleteAsync(CourseImage courseImage, bool saveChanges = true, 
+        CancellationToken cancellationToken = default)
+    {
+        return base.DeleteAsync(courseImage, saveChanges, cancellationToken);
+    }
+}
---
+++ src/Gymphony.Backend/Gymphony.Persistence/Repositories/Interfaces/ICourseImageRepository.cs
@@ -0,0 +1,16 @@
+﻿using Gymphony.Domain.Common.Queries;
+using Gymphony.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace Gymphony.Persistence.Repositories.Interfaces;
+
+public interface ICourseImageRepository
+{
+    IQueryable<CourseImage> Get(Expression<Func<CourseImage, bool>>? predicate = default, QueryOptions queryOptions = default);
+
+    ValueTask<CourseImage?> GetByIdAsync(Guid courseImageId, QueryOptions queryOptions = default, CancellationToken cancellationToken = default);
+
+    ValueTask<CourseImage> CreateAsync(CourseImage courseImage, bool saveChanges = true, CancellationToken cancellationToken = default);
+
+    ValueTask<CourseImage> DeleteAsync(CourseImage courseImage, bool saveChanges = true, CancellationToken cancellationToken = default);
+}
