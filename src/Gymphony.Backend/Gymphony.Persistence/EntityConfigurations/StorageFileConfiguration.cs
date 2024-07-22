f25f2b6b06f3a238700386c3ade9bbaac3ae1081
Ilxom <ilxomkarimjonov22@gmail.com>
2024-07-22 14:53:56 +0500

    Create storage file entity and configuration, storage file type enum

---
+++ src/Gymphony.Backend/Gymphony.Persistence/EntityConfigurations/StorageFileConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Gymphony.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Gymphony.Persistence.EntityConfigurations;
+
+public class StorageFileConfiguration : IEntityTypeConfiguration<StorageFile>
+{
+    public void Configure(EntityTypeBuilder<StorageFile> builder)
+    {
+        builder.HasKey(file => file.Id);
+
+        builder.Property(file => file.FileName).IsRequired();
+        builder.Property(file => file.Url).IsRequired();
+    }
+}
