1a77fa86a00392752d5301a7a9e2c1d76843eb82
Ilxom <ilxomkarimjonov22@gmail.com>
2024-07-22 14:57:39 +0500

    Create storage file settings and upload file info dto model and mappers

---
+++ src/Gymphony.Backend/Gymphony.Application/Common/StorageFiles/Models/Settings/AzureBlobStorageSettings.cs
@@ -0,0 +1,18 @@
+﻿using Gymphony.Domain.Enums;
+
+namespace Gymphony.Application.Common.StorageFiles.Models.Settings;
+
+public class AzureBlobStorageSettings
+{
+    public StorageFileType StorageFileType { get; set; }
+
+    public List<string> AllowedImageExtensions { get; set; } = default!;
+
+    public int MinimumImageSizeInBytes { get; set; }
+
+    public int MaximumImageSizeInBytes { get; set; }
+
+    public string ContainerName { get; set; } = default!;
+
+    public string VirtualPath { get; set; } = default!;
+}
