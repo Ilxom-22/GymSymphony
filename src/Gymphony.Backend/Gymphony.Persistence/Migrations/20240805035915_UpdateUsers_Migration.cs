a97c3b0c07129ee0a78377bf8eb8bb8a33920588
Ilxom <ilxomkarimjonov22@gmail.com>
2024-08-05 09:14:39 +0500

    Update users migration

---
+++ src/Gymphony.Backend/Gymphony.Persistence/Migrations/20240805035915_UpdateUsers_Migration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gymphony.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUsers_Migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Admin_TemporaryPasswordChanged",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Admin_TemporaryPasswordChanged",
+                table: "Users",
+                type: "boolean",
+                nullable: true);
+        }
+    }
+}
