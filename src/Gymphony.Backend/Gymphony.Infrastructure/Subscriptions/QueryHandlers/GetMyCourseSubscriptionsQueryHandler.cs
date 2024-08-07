666aaf26fb48d6ee14b1c5843903347f4c00484f
Ilxom <ilxomkarimjonov22@gmail.com>
2024-08-07 09:28:16 +0500

    Create get my course subscription query and handler

---
+++ src/Gymphony.Backend/Gymphony.Infrastructure/Subscriptions/QueryHandlers/GetMyCourseSubscriptionsQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Gymphony.Application.Subscriptions.Models.Dtos;
+using Gymphony.Application.Subscriptions.Queries;
+using Gymphony.Domain.Brokers;
+using Gymphony.Domain.Common.Queries;
+using Gymphony.Persistence.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Authentication;
+
+namespace Gymphony.Infrastructure.Subscriptions.QueryHandlers;
+
+public class GetMyCourseSubscriptionsQueryHandler(
+    IMapper mapper,
+    IRequestContextProvider requestContextProvider,
+    ICourseSubscriptionRepository courseSubscriptionRepository) 
+    : IQueryHandler<GetMyCourseSubscriptionsQuery, IList<CourseSubscriptionDto>>
+{
+    public async Task<IList<CourseSubscriptionDto>> Handle(GetMyCourseSubscriptionsQuery request, CancellationToken cancellationToken)
+    {
+        var memberId = requestContextProvider.GetUserIdFromClaims()
+            ?? throw new AuthenticationException("Unauthorized access!");
+
+        var courses = await courseSubscriptionRepository.Get(c => c.MemberId == memberId)
+            .Include(c => c.LastSubscriptionPeriod!)
+            .Include(c => c.Course!)
+            .ThenInclude(c => c.CourseImages!)
+            .ThenInclude(c => c.StorageFile)
+            .Where(c => c.LastSubscriptionPeriod!.ExpiryDate > DateOnly.FromDateTime(DateTime.UtcNow))
+            .ToListAsync(cancellationToken);
+
+        return mapper.Map<IList<CourseSubscriptionDto>>(courses);
+    }
+}
