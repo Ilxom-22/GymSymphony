4db9f577ec58dbe466447d84d66aa6e7e8343f1a
Ilxom <ilxomkarimjonov22@gmail.com>
2024-09-12 12:46:56 +0500

    Refactor course schedules handlers

--- src/Gymphony.Backend/Gymphony.Infrastructure/Courses/CommandHandlers/CreateCourseCommandHandler.cs
+++ src/Gymphony.Backend/Gymphony.Infrastructure/Courses/CommandHandlers/CreateCourseCommandHandler.cs
@@ -1,15 +1,18 @@
 ﻿using AutoMapper;
 using FluentValidation;
+using Gymphony.Application.Common.StorageFiles.Commands;
 using Gymphony.Application.Courses.Commands;
 using Gymphony.Application.Courses.Models.Dtos;
 using Gymphony.Domain.Common.Commands;
 using Gymphony.Domain.Entities;
 using Gymphony.Domain.Enums;
 using Gymphony.Persistence.Repositories.Interfaces;
+using MediatR;
 
 namespace Gymphony.Infrastructure.Courses.CommandHandlers;
 
-public class CreateCourseCommandHandler(IMapper mapper, IValidator<DraftCourseDto> validator, ICourseRepository courseRepository)
+public class CreateCourseCommandHandler(IMapper mapper, IMediator mediator, 
+    IValidator<DraftCourseDto> validator, ICourseRepository courseRepository)
     : ICommandHandler<CreateCourseCommand, CourseDto>
 {
     public async Task<CourseDto> Handle(CreateCourseCommand request, CancellationToken cancellationToken)
@@ -22,11 +25,29 @@ public class CreateCourseCommandHandler(IMapper mapper, IValidator<DraftCourseDt
         if (await courseRepository.CourseExistsAsync(request.Course.Name, cancellationToken))
             throw new ArgumentException($"Course with name '{request.Course.Name}' already exists!");
 
-        var course = mapper.Map<Course>(request.Course);
-        course.Status = ContentStatus.Draft;
+        await courseRepository.BeginTransactionAsync();
 
-        await courseRepository.CreateAsync(course, cancellationToken: cancellationToken);
+        try
+        {
+            var course = mapper.Map<Course>(request.Course);
+            course.Status = ContentStatus.Draft;
 
-        return mapper.Map<CourseDto>(course);
+            await courseRepository.CreateAsync(course, cancellationToken: cancellationToken);
+
+            var uploadCourseImageCommand = mapper.Map<UploadCourseImageCommand>(request.Course.CourseImage);
+            uploadCourseImageCommand.CourseId = course.Id;
+            var courseImage = await mediator.Send(uploadCourseImageCommand, cancellationToken);
+
+            var courseDto = mapper.Map<CourseDto>(course);
+            courseDto.Image = courseImage;
+            await courseRepository.CommitTransactionAsync();
+
+            return courseDto;
+        }
+        catch
+        {
+            await courseRepository.RollbackTransactionAsync();
+            throw;
+        }
     }
 }
