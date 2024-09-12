f001fc4d0473f084867e3af59952ec9e707344d7
Ilxom <ilxomkarimjonov22@gmail.com>
2024-09-12 12:44:20 +0500

    Refactor subscribe for course command handlers

--- src/Gymphony.Backend/Gymphony.Infrastructure/Subscriptions/CommandHandlers/SubscribeForCourseCommandHandler.cs
+++ src/Gymphony.Backend/Gymphony.Infrastructure/Subscriptions/CommandHandlers/SubscribeForCourseCommandHandler.cs
@@ -2,9 +2,11 @@
 using FluentValidation;
 using Gymphony.Application.Common.Payments.Commands;
 using Gymphony.Application.Common.Payments.Models.Dtos;
+using Gymphony.Application.Courses.Services;
 using Gymphony.Application.Subscriptions.Commands;
 using Gymphony.Domain.Brokers;
 using Gymphony.Domain.Common.Commands;
+using Gymphony.Domain.Common.Queries;
 using Gymphony.Domain.Entities;
 using Gymphony.Domain.Enums;
 using Gymphony.Persistence.Repositories.Interfaces;
@@ -20,6 +22,8 @@ public class SubscribeForCourseCommandHandler(
     ICourseScheduleRepository courseScheduleRepository,
     ICourseRepository courseRepository,
     ICourseSubscriptionRepository courseSubscriptionRepository,
+    ICourseScheduleEnrollmentRepository courseScheduleEnrollmentRepository,
+    ITimeService timeService,
     IValidator<SubscribeForCourseCommand> commandValidator,
     IMemberRepository memberRepository) : ICommandHandler<SubscribeForCourseCommand, CheckoutSessionDto>
 {
@@ -44,7 +48,7 @@ public class SubscribeForCourseCommandHandler(
             .FirstOrDefaultAsync(cancellationToken);
 
         if (subscription?.LastSubscriptionPeriod?.ExpiryDate > DateOnly.FromDateTime(DateTime.UtcNow))
-            throw new ArgumentException($"Member with Id {memberId} already has an active subscription for course with id { request.CourseId }");
+            throw new ArgumentException($"You already have an active subscription for this course.");
 
         var course = await courseRepository.Get(c => c.Id == request.CourseId)
             .Include(c => c.Schedules!)
@@ -58,7 +62,7 @@ public class SubscribeForCourseCommandHandler(
         if (course.Status != ContentStatus.Activated || course.StripeDetails is null)
             throw new ArgumentException($"Course with id {request.CourseId} is not available for subscription!");
 
-        var chosenSchedules = ValidateAndGetChosenSchedules(course, request.SchedulesIds);
+        var chosenSchedules = await  ValidateAndGetChosenSchedules(course, request.SchedulesIds, memberId, cancellationToken);
 
         var createCheckoutSessionCommand = mapper.Map<CreateCheckoutSessionCommand>(request);
         createCheckoutSessionCommand.Member = member;
@@ -83,7 +87,7 @@ public class SubscribeForCourseCommandHandler(
         return stripeCheckoutSession;
     }
 
-    private List<CourseSchedule> ValidateAndGetChosenSchedules(Course course, ICollection<Guid> scheduleIds)
+    private async ValueTask<List<CourseSchedule>> ValidateAndGetChosenSchedules(Course course, ICollection<Guid> scheduleIds, Guid memberId, CancellationToken cancellationToken)
     {
         var chosenSchedules = course.Schedules?
             .Where(s => scheduleIds.Contains(s.Id))
@@ -98,6 +102,27 @@ public class SubscribeForCourseCommandHandler(
                 throw new ArgumentException($"Course schedule with id {schedule.Id} has reached maximum enrollment.");
         }
 
+        // need to query the personal schedules and check if any of them overlap.
+        var personalSchedules = await courseScheduleEnrollmentRepository
+            .Get(cs => cs.MemberId == memberId, new QueryOptions(QueryTrackingMode.AsNoTracking))
+            .Include(cs => cs.CourseSchedule)
+            .Select(cs => cs.CourseSchedule)
+            .ToListAsync(cancellationToken);
+
+        if (AreSchedulesOverlapping(chosenSchedules, personalSchedules!))
+        {
+            throw new ArgumentException("Some of the chosen schedules are overlapping with the existing ones. Please check you personal calendar.");
+        }
+
         return chosenSchedules;
     }
+
+    public bool AreSchedulesOverlapping(List<CourseSchedule> chosenSchedules, List<CourseSchedule> personalSchedules)
+    {
+        return chosenSchedules.Any(schedule1 =>
+           personalSchedules.Any(schedule2 =>
+               timeService.IsTimeOverlapping(schedule1, schedule2)));
+    }
+
+
 }
